6b8ab6afc7635a7f86c8efde6a5a9c661ea98d26
goamegah <komlan.godwin.amegah@gmail.com>
2025-02-18 22:54:59 +0100

    tabnet + highwaynet

--- piu/tabnet.py
+++ piu/tabnet.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Charger les données
+
 train_df = pd.read_csv(f'{DATASET_PATH}/train.csv')
 test_df = pd.read_csv(f'{DATASET_PATH}/test.csv')
 
@@ -32,13 +32,12 @@ X, y, class_weights = preprocessor.fit_transform(train_df)
 
 print(f"\n*Nombre de features après transformation : {X.shape[1]}\n")
 
-# Stratified Split pour conserver la répartition des classes
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=(1 - 0.8), stratify=y, random_state=42
 )
-print(f"\n*Répartition des classes dans train : {np.bincount(y_train.numpy())}")
-print(f"*Répartition des classes dans test : {np.bincount(y_test.numpy())}")
-print(f"*Taille du train set: {len(y_train)}, Taille du test set: {len(y_test)}")
+print(f"* Répartition des classes dans train : {np.bincount(y_train.numpy())}")
+print(f"* Répartition des classes dans test : {np.bincount(y_test.numpy())}")
+print(f"* Taille du train set: {len(y_train)}, Taille du test set: {len(y_test)}")
 
 X_train = X_train.numpy()
 X_test = X_test.numpy()
